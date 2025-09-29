14e632bbaa5f0859d36205c2bab9abefe32371ff
compscibaddie <601335@student.belgiumcampus.ac.za>
2025-09-29 08:09:19 +0200

    Add Main App.tsx

--- portfolio-website/src/App.tsx
+++ portfolio-website/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import Statement from './components/Statement';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <Education />
+      <Skills />
+      <Statement />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
